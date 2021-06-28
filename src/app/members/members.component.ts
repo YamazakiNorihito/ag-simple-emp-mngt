b1f6c22c909c8b33377df1525c7a96f5690cca45
Yamazaki Norihito <kizinn1224@gmail.com>
2021-06-28 21:46:56 +0900

    リスト表示 \n https://angular.jp/tutorial/toh-pt2

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -2,21 +2,24 @@ import { Component, OnInit } from '@angular/core';
 
 import { Member } from '../member';
 
+import {MEMBERS} from '../mock-members'
+
 @Component({
   selector: 'app-members',  //  コンポーネントのCSS要素セレクター
   templateUrl: './members.component.html', //コンポーネントのテンプレートファイルの場所
   styleUrls: ['./members.component.css'] // コンポーネントのプライベートCSSスタイルの場所
 })
 export class MembersComponent implements OnInit {
+  members = MEMBERS;
 
-  member: Member = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedMember?: Member;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelect(mem: Member): void {
+    this.selectedMember = mem;
+  }
 }
---
+++ src/app/mock-members.spec.ts
@@ -0,0 +1,7 @@
+import { MockMembers } from './mock-members';
+
+describe('MockMembers', () => {
+  it('should create an instance', () => {
+    expect(new MockMembers()).toBeTruthy();
+  });
+});
